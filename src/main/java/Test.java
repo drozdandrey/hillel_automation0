1a6520dd064c285ce1e0068de036199d2a39127f
Rijkov <alexzhuravlov13@gmail.com>
2019-04-03 20:52:40 +0300

    Hillel

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://it-ebooks.info/");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
